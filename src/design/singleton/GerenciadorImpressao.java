fabf34a1db52cb6b19fc680a79fe7eb4ddb8efdf
NapoleaoHerculano <francivaldo.napoleao@gmail.com>
2020-03-13 12:41:40 -0300

    Singleton example done!

---
+++ src/design/singleton/GerenciadorImpressao.java
@@ -0,0 +1,18 @@
+package design.singleton;
+
+public class GerenciadorImpressao {
+
+    private static GerenciadorImpressao gerenciadorImpressao;
+
+    private GerenciadorImpressao(){
+
+    }
+
+    public static GerenciadorImpressao getInstance(){
+        if (gerenciadorImpressao == null){
+            gerenciadorImpressao = new GerenciadorImpressao();
+        }
+
+        return gerenciadorImpressao;
+    }
+}
---
+++ src/design/singleton/Main.java
@@ -0,0 +1,14 @@
+package design.singleton;
+
+public class Main {
+    public static void main(String[] args) {
+
+        GerenciadorImpressao g1 = GerenciadorImpressao.getInstance();
+        GerenciadorImpressao g2 = GerenciadorImpressao.getInstance();
+        GerenciadorImpressao g3 = GerenciadorImpressao.getInstance();
+
+        System.out.println(g1);
+        System.out.println(g2);
+        System.out.println(g3);
+    }
+}
